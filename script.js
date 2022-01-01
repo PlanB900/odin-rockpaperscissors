482369e0f109a6ec9eedc6a12808b150096ae7b0
PlanB900 <rythodir@gmail.com>
2022-01-01 14:13:15 -0700

    Rewrite logic to work with DOM elements, add style

--- script.js
+++ script.js
@@ -15,50 +15,167 @@ let c = 0
 let player
 let cpu
 
+let rockBtn = document.getElementById("rockBtn");
+let paperBtn = document.getElementById("paperBtn");
+let scissorsBtn = document.getElementById("scissorsBtn");
 
-while (p<5 && c<5){
-    console.log(`You:${p}  CPU:${c}`);
+let scoreTracker = document.getElementById("scoreTracker");
+let gameOver = document.getElementById("gameOver");
 
-    player = prompt("Enter rock, paper, or scissors.");
-    player = player.toUpperCase();
-    if (player != "ROCK" && player != "PAPER" && player != "SCISSORS"){
-        console.log("Please enter rock, paper, or scissors.");
-        continue;
-    }
+let buttons = document.querySelector('.buttons');
 
+let roundMessage = document.getElementById("roundMessage");
+
+//Variable for each button, to assign hover event
+let gameButtons = document.getElementsByClassName("btn");
+
+
+
+
+//Button hover event
+Array.from(gameButtons).forEach(function(x){
+    x.addEventListener('mouseover',(e)=>{
+        x.style.backgroundColor = "rgb("+e.offsetX+","
+        +e.offsetY+",0)";
+    });
+    x.addEventListener('mousedown',(e)=>{
+        x.classList.add("btnClick");
+    });
+    x.addEventListener('mouseup',(e)=>{
+        x.classList.remove("btnClick");
+    })
+});
+
+
+//Listen for button click, run game with respective button value
+rockBtn.addEventListener('click', ()=>{
+    player="ROCK"
+    console.log(player);
+    playRound(player);
+});
+paperBtn.addEventListener('click', ()=>{
+    player="PAPER"
+    console.log(player);
+    playRound(player);
+});
+scissorsBtn.addEventListener('click', ()=>{
+    player="SCISSORS"
+    console.log(player);
+    playRound(player);
+});
+
+
+
+
+//Generate CPU play 
+function getCPUSelection(){    
     cpu = getRandomInt(1,3)
     if (cpu==1){
         cpu = "ROCK";
+        return cpu;
     }else if(cpu==2){
         cpu = "PAPER";
+        return cpu;
     }else{
         cpu="SCISSORS";
-    }
-
-    if (player == "ROCK" && cpu == "PAPER"){
-        console.log("You lost the round!");
-        c++;
-    }else if(player == "ROCK" && cpu == "SCISSORS"){
-        console.log("You won the round!");
-        p++;
-    }else if(player == "PAPER" && cpu == "ROCK"){
-        console.log("You won the round!");
-        p++;
-    }else if(player == "PAPER" && cpu == "SCISSORS"){
-        console.log("You lost the round!");
-        c++;
-    }else if(player == "SCISSORS" && cpu == "ROCK"){
-        console.log("You lost the found!");
-        c++;
-    }else if(player=="SCISSORS" && cpu == "PAPER"){
-        console.log("You won the round!");
-        p++;
-    }else{
-        console.log("The round was a tie.");
-        continue;
+        return cpu;
     }
 }
 
-console.log(`Game over. The final score was \nYou:${p}  CPU:${c}`);
+//Screen to show when game ends, ask if play again
+function gameOverScreen(){
+    buttons.style.display = "none";
+
+    //Insert play again button
+    let playAgainBtn = document.createElement('button');
+    playAgainBtn.className = "btn";
+    playAgainBtn.textContent = "Play again";
+    gamecontainer.insertBefore(playAgainBtn, gamecontainer.firstChild);
+
+    playAgainBtn.addEventListener('click', ()=>{
+        buttons.style.display = "flex";
+        playAgainBtn.style.display = "none";
+        roundMessage.textContent = "";
+        c=0;
+        p=0;
+        scoreTracker.textContent = `The current score is
+        CPU: ${c} You: ${p}`;
+    })
+}
+
+//Check score to see if game is over and update score on page
+function checkScore(){
+    if(c==5){
+        scoreTracker.textContent = `You lose!
+        The final score is CPU:${c} YOU:${p}`;
+        gameOverScreen();
+        c=0;
+        p=0;
+        return;
+    }else if(p==5){
+        scoreTracker.textContent = `You win!
+        The final score is CPU:${c} YOU:${p}`;
+        gameOverScreen();
+        c=0;
+        p=0;
+        return;
+    }
+    scoreTracker.textContent = `The current score is CPU:${c} YOU:${p}`;
+
+}
+//Play round
+function playRound(playerSelection){   
+    let CPUSelection = getCPUSelection();
+
+    if (playerSelection == "ROCK" && CPUSelection == "PAPER"){
+        roundMessage.textContent = "You lost the round!";
+        ++c;
+        checkScore();
+    }else if(playerSelection == "ROCK" && CPUSelection == "SCISSORS"){
+        roundMessage.textContent = "You won the round!";
+        ++p;
+        checkScore();
+    }else if(playerSelection == "PAPER" && CPUSelection == "ROCK"){
+        roundMessage.textContent = "You won the round!";
+        ++p;
+        checkScore();
+    }else if(playerSelection == "PAPER" && CPUSelection == "SCISSORS"){
+        roundMessage.textContent = "You lost the round!";
+        ++c;
+        checkScore();
+    }else if(playerSelection == "SCISSORS" && CPUSelection == "ROCK"){
+        roundMessage.textContent = "You lost the round!";
+        ++c;
+        checkScore();
+    }else if(playerSelection=="SCISSORS" && CPUSelection == "PAPER"){
+        roundMessage.textContent = "You won the round!";
+        ++p;
+        checkScore();
+    }else if(playerSelection==CPUSelection){
+        roundMessage.textContent = "The round was a tie.";
+        checkScore();
+    }
+    
+}
+
+
+
+
+
+
+// console.log(`Game over. The final score was \nYou:${p}  CPU:${c}`);
+
+/*
+Three buttons:
+ROCK, PAPER, SCISSORS
+
+When the player clicks one, listen for a 'click' event,  and run a
+function that assigns respective value to the player selection variable.
+
+We also have a function that, when called, returns a value for the
+CPU play.
+
+Now, we have two values .
+*/
 
 
